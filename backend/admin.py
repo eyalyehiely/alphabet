49d52f81836f1f61d833b9e094eb6834666552fd
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 14:14:07 +0300

    configure admin dashboard

--- backend/admin.py
+++ backend/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import *
+from .models import User, Event, UserEvent
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'password', 'created_at', 'updated_at')
+    search_fields = ('email',)
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id','name','starting_time','end_time','location', 'created_at', 'updated_at')
-admin.site.register(Event, EventAdmin)
-
-
-class userAdmin(admin.ModelAdmin):
-    list_display = ('id','email','password','end_time','events', 'created_at', 'updated_at')
-admin.site.register(User, userAdmin)
+    list_display = ('id', 'name', 'starting_time', 'end_time', 'location', 'created_at', 'updated_at')
+    search_fields = ('name', 'location')
 
 class UserEventAdmin(admin.ModelAdmin):
-    list_display = ('user','event', 'created_at', 'updated_at')
+    list_display = ('user', 'event', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'event__name')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Event, EventAdmin)
 admin.site.register(UserEvent, UserEventAdmin)
