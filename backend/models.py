bdb05ac4c05b625153de4bd30d00754b80c60349
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 15:09:52 +0300

    change models

--- backend/models.py
+++ backend/models.py
@@ -8,23 +8,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    events = models.ManyToManyField('Event', through='UserEvent', related_name='users')
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='backend_users',  # Add a unique related_name
-        blank=True,
-        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
-        related_query_name='user',
-    )
-
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='backend_users',  # Add a unique related_name
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_query_name='user',
-    )
+    
     
     def __str__(self):
         return self.username
@@ -35,20 +19,9 @@ class Event(models.Model):
     starting_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.CharField(max_length=200)
+    participants = models.ManyToManyField(User, related_name='participating_events', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-class UserEvent(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ('user', 'event')
-
-    def __str__(self):
-        return f'{self.user.username} - {self.event.name}'
