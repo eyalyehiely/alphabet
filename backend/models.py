5076b5ffe837653d1a2665c8b0928ab2514cf743
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 13:36:42 +0300

    create user&event&userEvent models

--- backend/models.py
+++ backend/models.py
@@ -1,22 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-
-class User(models.Model):
-    id = models.UUIDField()
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length = 200)
-    events = models.ForeignKey(Event,on_delete=models.CASCADE, related_name='user')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    events = models.ManyToManyField('Event', through='UserEvent', related_name='users')
 
-    
+    def __str__(self):
+        return self.username
 
 class Event(models.Model):
-    id = models.UUIDField()
-    name = models.CharField(max_length = 200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
     starting_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    location = models.CharField(max_length = 200)
+    location = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class UserEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'event')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.event.name}'
