80873f3bd5f3689698a797aa7c7ada5f86fb04f8
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 14:14:18 +0300

    update models

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,31 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     events = models.ManyToManyField('Event', through='UserEvent', related_name='users')
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='backend_users',  # Add a unique related_name
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_query_name='user',
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='backend_users',  # Add a unique related_name
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_query_name='user',
+    )
+    
     def __str__(self):
         return self.username
 
