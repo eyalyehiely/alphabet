e97fac63746f057b6f18229a5772a1582062245d
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 15:17:20 +0300

    update admin dashboard

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Event, UserEvent
+from .models import User, Event
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'password', 'created_at', 'updated_at')
@@ -9,10 +9,6 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'starting_time', 'end_time', 'location', 'created_at', 'updated_at')
     search_fields = ('name', 'location')
 
-class UserEventAdmin(admin.ModelAdmin):
-    list_display = ('user', 'event', 'created_at', 'updated_at')
-    search_fields = ('user__username', 'event__name')
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Event, EventAdmin)
-admin.site.register(UserEvent, UserEventAdmin)
