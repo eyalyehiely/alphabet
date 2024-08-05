3a1bf73bfba068e87086f018eeee288c5b6eb708
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 14:38:10 +0300

    create an event view

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('events/', event, name='event_list'),
-    path('events/<uuid:id>/', event, name='get_specific_event'),
+    path('events/', events, name='event_list'),
+    path('event/<uuid:id>/', event, name='event_detail'),
+    # path('users/', UserListCreate.as_view(), name='user-list-create'),
+    # path('users/<uuid:id>/', UserDetail.as_view(), name='user-detail'),
    
 ]
--- backend/views.py
+++ backend/views.py
@@ -9,7 +9,7 @@ import logging
 events_logger = logging.getLogger(__name__)
 
 @api_view(['GET', 'POST'])
-def event(request, name, location, starting_time, end_time ):
+def events(request, name, location, starting_time, end_time ):
     try:
         # Return all events
         if request.method == 'GET':
@@ -24,7 +24,6 @@ def event(request, name, location, starting_time, end_time ):
         
         # Create a new event
         if request.method == 'POST':
-            data = request.data
             new_event = Event.objects.create(
                 id=uuid.uuid4(),
                 name= name,
@@ -35,6 +34,59 @@ def event(request, name, location, starting_time, end_time ):
             new_event.save()
             events_logger.debug('New event saved')
             return Response({'message': 'Event created successfully'}, status=201)
+    except Exception as e:
+        events_logger.error('Error occurred: %s', e)
+        return Response({'error': str(e)}, status=500)
+    
+
+
+
+
+@api_view(['GET','PUT','DELETE'])
+def event(request, name, location, starting_time, end_time,id):
+    try:
+        # Return specific event
+        if request.method == 'GET':
+            event = Event.objects.filter(id=id).first()
+            if event.exists():
+                events_logger.debug('Event found')
+                return Response({'event': event}, status=200)
+            else:
+                events_logger.debug('No events found')
+                return Response({'message': 'No events found'}, status=404)
+        
+        # update event
+        if request.method == 'PUT':
+            try:
+                event = Event.objects.filter(id=id).first()
+                if event():
+                    event.id = uuid.uuid4(),
+                    event.name = name,
+                    event.starting_time = starting_time,
+                    event.end_time = end_time,
+                    event.location = location
+                    event.save()
+                    events_logger.debug('Event has been updated')
+                    return Response({'message': 'Event has been updated'}, status=200)
+                else:
+                    events_logger.debug('No event found')
+                    return Response({'message': 'No event found'}, status=404)
+                
+            except (ValueError, TypeError) as e:
+                events_logger.debug(f'Invalid data received: {e}')
+                return Response({'error': str(e)}, status=400)
+
+        
+        if request.method == 'DELETE':
+            event = Event.objects.filter(id=id).first()
+            if event.exists():
+                events_logger.debug('Event found')
+                event.delete()
+                return Response({'message': 'Event deleted'}, status=200)
+            else:
+                events_logger.debug('No events found')
+                return Response({'message': 'No events found'}, status=404)
+            
     except Exception as e:
         events_logger.error('Error occurred: %s', e)
         return Response({'error': str(e)}, status=500)
