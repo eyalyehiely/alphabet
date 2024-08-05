00329480078ee3a5937dfef606fdb77df980d699
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 13:37:51 +0300

    add models to admin dash

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id','name','starting_time','end_time','location', 'created_at', 'updated_at')
+admin.site.register(Event, EventAdmin)
+
+
+class userAdmin(admin.ModelAdmin):
+    list_display = ('id','email','password','end_time','events', 'created_at', 'updated_at')
+admin.site.register(User, userAdmin)
+
+class UserEventAdmin(admin.ModelAdmin):
+    list_display = ('user','event', 'created_at', 'updated_at')
+admin.site.register(UserEvent, UserEventAdmin)
