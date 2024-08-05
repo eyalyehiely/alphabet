61afaa108c918faa1474f998037180703657a239
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 20:28:09 +0300

    add participants to admin panel

--- backend/admin.py
+++ backend/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import User, Event
+from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'starting_time', 'end_time', 'location', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'starting_time', 'end_time', 'location', 'created_at', 'updated_at', 'display_participants')
     search_fields = ('name', 'location')
     filter_horizontal = ('participants',)
+
+    def display_participants(self, obj):
+        return ", ".join([participant.username for participant in obj.participants.all()])
+    display_participants.short_description = 'Participants'
 
 admin.site.register(Event, EventAdmin)
