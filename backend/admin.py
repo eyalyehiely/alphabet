1a14c46e129965b3e0848cae5757affbf8bd8b1e
eyalyehiely <eyalwork0@gmail.com>
2024-08-05 15:32:33 +0300

    removew user model

--- backend/admin.py
+++ backend/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from .models import User, Event
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'password', 'created_at', 'updated_at')
-    search_fields = ('email',)
-
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'starting_time', 'end_time', 'location', 'created_at', 'updated_at')
     search_fields = ('name', 'location')
 
-
-admin.site.register(User, UserAdmin)
 admin.site.register(Event, EventAdmin)
--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,6 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    
-    def __str__(self):
-        return self.username
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
