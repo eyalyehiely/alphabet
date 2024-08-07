af9e822584108df99729fdc6f19867f13b598ecc
eyalyehiely <eyalwork0@gmail.com>
2024-08-07 16:36:06 +0300

    update eventserializers

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+import pytz
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -16,7 +17,18 @@ class EventSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'starting_time', 'end_time', 'location', 'participants', 'created_at', 'updated_at']
 
 
+    def get_participants(self, obj):
+        return [participant.username for participant in obj.participants.all()]
+    
+    def get_created_at(self, obj):
+        israel_tz = pytz.timezone('Asia/Jerusalem')
+        created_at_israel = obj.created_at.astimezone(israel_tz)
+        return created_at_israel.strftime('%d/%m/%Y %H:%M')
 
+    def get_updated_at(self, obj):
+        israel_tz = pytz.timezone('Asia/Jerusalem')
+        updated_at_israel = obj.updated_at.astimezone(israel_tz)
+        return updated_at_israel.strftime('%d/%m/%Y %H:%M')
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
