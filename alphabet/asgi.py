d54bbb786f8a194fc380a4563283013507fb5ffb
eyalyehiely <eyalwork0@gmail.com>
2024-08-07 14:45:37 +0300

    add asgi.py file

--- alphabet/asgi.py
+++ alphabet/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for alphabet project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+# alfabet_backend/asgi.py
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import backend.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alphabet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alfabet_backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            backend.routing.websocket_urlpatterns
+        )
+    ),
+})
