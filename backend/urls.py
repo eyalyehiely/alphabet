6a874032a781401403310fe58bec9cdeeaf2f522
eyalyehiely <eyalwork0@gmail.com>
2024-08-07 14:46:36 +0300

    fix routes for better looking swagger

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('event/<uuid:id>/', event, name='event_detail'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signin/', signin, name='signin'),
-    path('signup/', signup, name='signup'),
-    path('users/', users, name='event_list'),
+    path('auth/signin/', signin, name='signin'),
+    path('auth/signup/', signup, name='signup'),
+    path('users/', users, name='users_list'),
     path('user/<uuid:id>/', user, name='user_detail'),   
 ]
