2ba6fd3fb3b05a83f3fa4868bbfd7e0ab21caf08
eyalyehiely <eyalwork0@gmail.com>
2024-08-08 14:00:40 +0300

    add logs on consumers.py

--- backend/consumers.py
+++ backend/consumers.py
@@ -1,13 +1,16 @@
-# events/consumers.py
+# backend/consumers.py
 import json
+import logging
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
-from .models import Event
+
+socket_logger = logging.getLogger("socket")
 
 class EventConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.event_id = self.scope['url_route']['kwargs']['event_id']
         self.event_group_name = f'event_{self.event_id}'
+
+        socket_logger.info(f'Connecting to WebSocket: event_id={self.event_id}, event_group_name={self.event_group_name}')
 
         # Join event group
         await self.channel_layer.group_add(
@@ -16,15 +19,22 @@ class EventConsumer(AsyncWebsocketConsumer):
         )
 
         await self.accept()
+        socket_logger.info(f'Successfully connected to WebSocket: event_id={self.event_id}')
 
     async def disconnect(self, close_code):
+        socket_logger.info(f'Disconnecting from WebSocket: event_id={self.event_id}, close_code={close_code}')
+
         # Leave event group
         await self.channel_layer.group_discard(
             self.event_group_name,
             self.channel_name
         )
 
+        socket_logger.info(f'Successfully disconnected from WebSocket: event_id={self.event_id}')
+
     async def receive(self, text_data):
+        socket_logger.info(f'Received message on WebSocket: event_id={self.event_id}, message={text_data}')
+
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
@@ -39,12 +49,9 @@ class EventConsumer(AsyncWebsocketConsumer):
 
     async def event_message(self, event):
         message = event['message']
+        socket_logger.info(f'Sending message to WebSocket: event_id={self.event_id}, message={message}')
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
-
-    @sync_to_async
-    def get_event(self):
-        return Event.objects.get(id=self.event_id)
