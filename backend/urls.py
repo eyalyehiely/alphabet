6406a5ea54939a5028879eed79b991fd34d821b5
eyalyehiely <eyalwork0@gmail.com>
2024-08-08 18:04:23 +0300

    delete users and add search event

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('event/<uuid:id>/', event, name='event_detail'),
     path('auth/signin/', signin, name='signin'),
     path('auth/signup/', signup, name='signup'),
-    path('users/', users, name='users_list'),
     path('user/<int:user_id>/', user_detail, name='user_detail'), 
+    path('search_event/<str:location>/', search_event, name='search_event'), 
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
 ]
