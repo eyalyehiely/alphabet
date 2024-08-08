da1e0a21f2f4dffbf53862685d6f6ffddd9f919e
eyalyehiely <eyalwork0@gmail.com>
2024-08-08 14:42:24 +0300

    change user view name

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('auth/signin/', signin, name='signin'),
     path('auth/signup/', signup, name='signup'),
     path('users/', users, name='users_list'),
-    path('user/<uuid:id>/', user, name='user_detail'), 
+    path('user/<int:user_id>/', user_detail, name='user_detail'), 
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
 ]
